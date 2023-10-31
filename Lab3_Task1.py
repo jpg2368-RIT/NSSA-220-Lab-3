beaf60b59760915862b2290889defdcb4838a33a
School <jpg2368@rit.edu>
2023-10-31 14:14:25 -0400

    Refactored code, renamed variables

--- Lab3_Task1.py
+++ Lab3_Task1.py
@@ -1,99 +1,106 @@
 #!/bin/python3
 
 import sys
-#from dataclasses import dataclass
+from dataclasses import dataclass
 
+
+@dataclass
 class Iris:
-	irisType: str
-	sLen: float
-	sWid: float
-	pLen: float
-	pWid: float
-	
-	def __init__(self, iType, iSLen, iSWid, iPLen, iPWid):
-		self.irisType = iType
-		self.sLen = iSLen
-		self.sWid = iSWid
-		self.pLen = iPLen
-		self.pWid = iPWid
+    irisType: str
+    sLen: float
+    sWid: float
+    pLen: float
+    pWid: float
 
-def readData(inFile):
-	irisList = []
-	reading = False
-	with open(inFile, "r") as file:
-		for line in file:
-			if "@Data" in line:
-				reading = True
-				continue
-			if reading:
-				fields = line.split(',')
-				if fields[4] == "Iris-setosa":
-					irisType = "setosa"
-				elif fields[4] == "Iris-virginica":
-					irisType = "virginica"
-				elif fields[4] == "Iris-versicolor":
-					irisType = "versicolor"
-				else:
-					irisType = "unknown"
-				irisList.append(Iris(irisType, float(fields[0]), float(fields[1]), float(fields[2]), float(fields[3])))
-	return irisList		
-	
+    def __init__(self, iris_type, s_len, s_wid, p_len, p_wid):
+        self.irisType = iris_type
+        self.sLen = s_len
+        self.sWid = s_wid
+        self.pLen = p_len
+        self.pWid = p_wid
+
+
+def readData(in_file):
+    iris_list = []
+    reading = False
+    with open(in_file) as file:
+        for line in file:
+            if "@Data" in line:
+                reading = True
+                continue
+            if reading:
+                fields = line.split(',')
+                if fields[4] == "Iris-setosa":
+                    iris_type = "setosa"
+                elif fields[4] == "Iris-virginica":
+                    iris_type = "virginica"
+                elif fields[4] == "Iris-versicolor":
+                    iris_type = "versicolor"
+                else:
+                    iris_type = "unknown"
+                iris_list.append(
+                    Iris(iris_type, float(fields[0]), float(fields[1]), float(fields[2]), float(fields[3])))
+    return iris_list
+
+
 def procNumField(iList, field):
-	vals = []
-	for iris in iList:
-		if field == 1:
-			vals.append(iris.sLen)
-		elif field == 2:
-			vals.append(iris.sWid)
-		elif field == 3:
-			vals.append(iris.pLen)
-		elif field == 4:
-			vals.append(iris.pWid)
-		else:
-			print(f"Field {field} is invalid")
-			exit(1)
-	fieldMin = min(vals)
-	fieldMax = max(vals)
-	fieldAvg = sum(vals)/len(vals)
-	return fieldMin, fieldMax, fieldAvg
-	
-def countIrisTypes(iList):
-	setCount = 0
-	virCount = 0
-	verCount = 0
-	for iris in iList:
-		iType = iris.irisType
-		if iType == "setosa":
-			setCount += 1
-		elif iType == "virginica":
-			virCount += 1
-		elif iType == "versicolor":
-			verCount += 1
-		else:
-			print(f"Iris type {iType} is unknown")
-			exit(1)
-	return setCount, virCount, verCount
+    vals = []
+    for iris in iList:
+        if field == 1:
+            vals.append(iris.sLen)
+        elif field == 2:
+            vals.append(iris.sWid)
+        elif field == 3:
+            vals.append(iris.pLen)
+        elif field == 4:
+            vals.append(iris.pWid)
+        else:
+            print(f"Field {field} is invalid")
+            exit(1)
+    field_min = min(vals)
+    field_max = max(vals)
+    field_avg = sum(vals) / len(vals)
+    return field_min, field_max, field_avg
+
+
+def countIrisTypes(iris_list):
+    set_count = 0
+    vir_count = 0
+    ver_count = 0
+    for iris in iris_list:
+        iris_type = iris.irisType
+        if iris_type == "setosa":
+            set_count += 1
+        elif iris_type == "virginica":
+            vir_count += 1
+        elif iris_type == "versicolor":
+            ver_count += 1
+        else:
+            print(f"Iris type {iris_type} is unknown")
+            exit(1)
+    return set_count, vir_count, ver_count
+
 
 def main(args):
-	if len(args) != 2:
-		print(f"Incorrect number of arguments. Expected 1, got {len(args)}")
-		exit(1)
-	
-	irisList = readData(args[1])
-	print(f"{irisList}")
-	
-	#fieldMin, fieldMax, fieldAvg = procNumField(irisList, 1)
-	#print(f"Sepal Length: min = {fieldMin}, max = {fieldMax}, average = {fieldAvg}")
-	#fieldMin, fieldMax, fieldAvg = procNumField(irisList, 2)
-	#print(f"Sepal Width: min = {fieldMin}, max = {fieldMax}, average = {fieldAvg}")
-	#fieldMin, fieldMax, fieldAvg = procNumField(irisList, 3)
-	#print(f"Petal Length: min = {fieldMin}, max = {fieldMax}, average = {fieldAvg}")
-	#fieldMin, fieldMax, fieldAvg = procNumField(irisList, 4)
-	#print(f"Petal Width: min = {fieldMin}, max = {fieldMax}, average = {fieldAvg}")
-	
-	numSet, numVir, numVer = countIrisTypes(irisList)
-	print(f"Iris Types: Iris Steosa = {numSet}, Iris Versicolor = {numVer}, Iris Virginica = {numVir}")		
+    if len(args) != 2:
+        print(f"Incorrect number of arguments. Expected 2, got {len(args)}")
+        exit(1)
 
-	
+    iris_list = readData(args[1])
+    print(f"{iris_list}")
+
+    field_min, field_max, field_avg = procNumField(iris_list, 1)
+    print(f"Sepal Length: min = {field_min}, max = {field_max}, average = {field_avg}")
+    field_min, field_max, field_avg = procNumField(iris_list, 2)
+    print(f"Sepal Width: min = {field_min}, max = {field_max}, average = {field_avg}")
+    field_min, field_max, field_avg = procNumField(iris_list, 3)
+    print(f"Petal Length: min = {field_min}, max = {field_max}, average = {field_avg}")
+    field_min, field_max, field_avg = procNumField(iris_list, 4)
+    print(f"Petal Width: min = {field_min}, max = {field_max}, average = {field_avg}")
+
+    num_set, num_vir, num_ver = countIrisTypes(iris_list)
+    print(f"Iris Types: Iris Steosa = {num_set}, Iris Versicolor = {num_ver}, Iris Virginica = {num_vir}")
+
+
 if __name__ == '__main__':
-	main(sys.argv);
+    main(sys.argv)
